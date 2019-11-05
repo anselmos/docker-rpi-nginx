405a5039d7b22b63b9431a80324522c759d4b49e
Anselmos <anselmos@users.noreply.github.com>
2019-11-05 21:46:05 +0100

    Adds Dockerfile, Readme and build.sh

---
+++ README.md
@@ -0,0 +1,5 @@
+# Docker RPI nginx for armv6l Raspberry PI
+
+This is an unofficial nginx docker image that works on raspberry pi (armv6l).
+
+Uses Hypriot Rpi official Docker image.
---
+++ build.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# build.sh
+# Copyright (C) 2019 [Anselmos](github.com/anselmos) <anselmos@users.noreply.github.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+docker build -t anselmos/rpi-nginx .
